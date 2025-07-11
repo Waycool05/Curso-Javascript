1a4335243fa216ae1801e24b75366654d614ff7a
Jorge Adolfo Leitner <waycool05@hotmail.com>
2025-07-10 21:17:51 -0300

    version 3

---
+++ Unidad 2 - Fundamentos Javascript/Ejercicios Semana 2/4 - Simulador cajero automático (retiro simple) - Define saldo Inicial/script.js
@@ -0,0 +1,27 @@
+const saldoInicial = 100000;
+
+let saldoCapital= saldoInicial;
+
+for(;;){
+    let retiroDinero=(prompt(`Ingrese un monto a retirar (su saldo actual es ${saldoCapital}`));
+    if (retiroDinero===null) {
+        alert(`Operación Cancelada`);
+        break;
+    }
+    retiroDinero=parseInt(retiroDinero);
+    if (isNaN(retiroDinero)|| retiroDinero<=0) {
+        alert("ingrese un monto válido");
+        continue;
+    }
+    if (retiroDinero>saldoCapital) {
+        alert(`Por favor ingrese un valor menor, su saldo actual es: ${saldoCapital}`);
+    } else {
+        saldoCapital-= retiroDinero;
+        alert(`Su nuevo saldop es: ${saldoCapital}`);
+    }   
+    if (saldoCapital<=0) {
+        alert(`no hay saldo suficiente para continuar`);
+        break;
+    }
+}
+  
