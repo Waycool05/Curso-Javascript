2900c010cfcf14c9ba141f04d231303428e8c650
Jorge Adolfo Leitner <waycool05@hotmail.com>
2025-07-11 08:48:17 -0300

    version 3

--- Unidad 2 - Fundamentos Javascript/Ejercicios Semana 2/5 - Bloqueo por pin incorrecto - Define un PIN correcto/script.js
+++ Unidad 2 - Fundamentos Javascript/Ejercicios Semana 2/5 - Bloqueo por pin incorrecto - Define un PIN correcto/script.js
@@ -1,32 +1,27 @@
 const pinCorrecto = "6749";
 
-let intentos = 3;
-
-for(intentos=0;intentos<3;intentos++) {
+for(i=0;i<3;i++) {
 
     let ingresoNúmero=(prompt("Ingrese Pin de 4 Dígitos: "));
 
     if (ingresoNúmero===null) {
         alert("Operación Cancelada");
         break;
-    }
+    } 
     
     if (isNaN(ingresoNúmero) || ingresoNúmero.length!==4) {
-            alert(`Por favor ingrese un Pin Válido de 4 Números, le quedan ${intentos} intentos`);
+            alert(`Por favor ingrese un Pin Válido de 4 Números, le quedan ${2-i} intentos`);
             continue;
     }
     if (ingresoNúmero!==pinCorrecto){
-        alert(`Pin Incorrecto, le quedan ${2-intentos} Intentos.`)
+        alert(`Pin Incorrecto, le quedan ${2-i} Intentos.`)
         continue
     } 
-
-    
     if (ingresoNúmero===pinCorrecto){
         alert(`Pin Correcto!, puede continuar....`);
         break;
-    } else {
-        alert(`El pin ${ingresoNúmero} no es correcto, se abortará la Operación`);
-        break;
-    }
-
+    } 
+}
+if (i===3) {
+    alert ("atención, usuario bloqueado.")
 }
