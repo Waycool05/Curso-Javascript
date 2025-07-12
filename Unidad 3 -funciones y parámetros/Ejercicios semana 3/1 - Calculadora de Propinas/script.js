66669e00ac493e4a7d49c6ad1913c7fe920a92be
Jorge Adolfo Leitner <waycool05@hotmail.com>
2025-07-12 06:46:05 -0300

    version 3

--- Unidad 3 -funciones y parámetros/Ejercicios semana 3/1 - Calculadora de Propinas/script.js
+++ Unidad 3 -funciones y parámetros/Ejercicios semana 3/1 - Calculadora de Propinas/script.js
@@ -16,8 +16,32 @@ function totalconPropina(totalCuenta, porcentajePropina) {
     return totalaPagar;
 }
 
-let cuenta=parseFloat(prompt("Ingrese el total de la cuenta"));
-let propina=parseFloat(prompt("Ingrese el % de propina que desea dejar"));
+let cuenta=(prompt("Ingrese el total de la cuenta"));
+if (cuenta===null) {
+    alert("Ingrese un valor válido");
+} else {
+    cuenta = Number(cuenta);
+}
+let propina=prompt(
+    "Ingrese el % de propina que quiere dejar \n" +
+    "1 - 5%\n" +
+    "2 - 10%\n" +
+    "3 - 15%");
+switch(propina) {
+    case "1":
+        propina=5;
+        break;
+    case "2":
+        propina=10;
+        break;
+    case "3": 
+        propina=15;
+        break;
+    default: 
+        alert("por favor ingrese una Opcion válida");
+        propina=NaN;
+        break;
+}
 
 if (isNaN(cuenta) || cuenta<=0 || isNaN(propina) || propina<=0) {
     alert("por favor ingrese un valor válido");
