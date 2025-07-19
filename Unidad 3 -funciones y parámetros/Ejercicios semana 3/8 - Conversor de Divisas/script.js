84c7f11392a62c18c582f999d04dd0e9b2b0832f
Jorge Adolfo Leitner <waycool05@hotmail.com>
2025-07-19 17:45:24 -0300

    version 3

---
+++ Unidad 3 -funciones y parámetros/Ejercicios semana 3/8 - Conversor de Divisas/script.js
@@ -0,0 +1,12 @@
+// ### Ejercicio 8: Conversor de divisas
+
+// > Convierte pesos a otra moneda (USD, EUR, CLP) según tasa fija.
+// > 
+// > 
+// > El usuario elige la moneda.
+// > 
+// > Función devuelve el monto convertido.
+// >
+
+
+
