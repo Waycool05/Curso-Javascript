59f772400de9087e72d5bf9c4382fede9471068e
Jorge Adolfo Leitner <waycool05@hotmail.com>
2025-07-21 20:45:29 -0300

    version 4

---
+++ Unidad 4 -Arrays y objetos/Ejercicios/1 - Inventario de productos/script.js
@@ -0,0 +1,8 @@
+// ### Ejercicio 1: Inventario de productos
+
+// > Usa un array de objetos para representar productos (nombre, stock).
+// > 
+// > 
+// > Crea funciones para agregar, eliminar y mostrar productos.
+// >
+
