ab801c9ba6f26597babfec7ccbcb977d89bae4d3
Jorge Adolfo Leitner <waycool05@hotmail.com>
2025-07-23 21:19:33 -0300

    version 4

--- Unidad 4 -Arrays y objetos/Ejercicios/3 - Promedio de calificaciones/script.js
+++ Unidad 4 -Arrays y objetos/Ejercicios/3 - Promedio de calificaciones/script.js
@@ -5,3 +5,23 @@
 // > 
 // > Función que calcule y devuelva el promedio.
 // >
+
+// Array con notas numéricas
+const notas = [7, 8, 6, 9, 10, 5, 8];
+
+// Función que calcula y devuelve el promedio
+function calcularPromedio(arrayNotas) {
+    let suma = 0;
+
+    for (let i = 0; i < arrayNotas.length; i++) {
+        suma += arrayNotas[i]; // Acumulamos cada nota
+    }
+
+    const promedio = suma / arrayNotas.length;
+    return promedio; // Devuelve el promedio calculado
+}
+
+// Ejecutar y mostrar el resultado
+const promedioFinal = calcularPromedio(notas);
+console.log("El promedio de las calificaciones es: " + promedioFinal.toFixed(2));
+alert("El promedio de las calificaciones es: " + promedioFinal.toFixed(2) + " \nlas notas promediadas fueron: " + notas.join(", "));
