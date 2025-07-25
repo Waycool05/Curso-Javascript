decb19f0ed26ffaadf66661e7c6f51a4425d2bfa
Jorge Adolfo Leitner <waycool05@hotmail.com>
2025-07-25 19:24:46 -0300

    version 4

--- Unidad 4 -Arrays y objetos/Ejercicios/8 - Contador de elementos/script.js
+++ Unidad 4 -Arrays y objetos/Ejercicios/8 - Contador de elementos/script.js
@@ -5,3 +5,36 @@
 // > 
 // > Función que devuelva cuántas veces aparece cada una (objeto con frecuencia).
 // >
+const palabras = [
+    "JavaScript", "HTML", "CSS", "JavaScript",
+    "Python", "css", "HTML", "javascript",
+    "Node", "Python", "python"
+];
+
+function contarFrecuencia(arrPalabras) {
+    const frecuencia = {};
+    const palabrasMostradas= [];
+    let salida="";
+    for (let i = 0; i < arrPalabras.length; i++) {
+        const palabra = arrPalabras[i].toLowerCase();
+
+        if (frecuencia[palabra]) {
+            frecuencia[palabra]++;
+        } else {
+            frecuencia[palabra] = 1;
+        }
+    }
+
+    for (let i=0;i<arrPalabras.length;i++) {
+        let palabra=arrPalabras[i].toLowerCase();
+        if (!palabrasMostradas.includes(palabra)){
+            salida+= `${palabra} - ${frecuencia[palabra]}\n`;
+            palabrasMostradas.push(palabra);
+        
+        }
+    }
+    return salida;
+}
+
+const resultado = contarFrecuencia(palabras);
+alert(resultado);
